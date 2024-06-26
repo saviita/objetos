a04c0875931ba591192f4535e3c426f67b349c2d
saviita <taatt2000@gmail.com>
2024-06-26 10:16:39 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// OBJETOS
+
+const user = {
+  name: 'César',
+  age: 79,
+  isActive: false,
+  friends: [],
+  address: {
+    city: 'Albacete'
+  }
+};
+
+user.friends.push('globo');
+console.log(user);
